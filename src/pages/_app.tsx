8d7ff5cc635c63565acbca61c8659a502f5e7041
Luan Vendt <luanvendt@gmail.com>
2024-01-16 16:08:25 -0300

    Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
-
+import logoImg from '@/assets/logo.svg'
 import { globalStyles } from "@/styles/global"
+import { Container, Header } from '@/styles/pages/app'
 import { AppProps } from "next/app"
 
 
 globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps }/>
+    return (
+        <Container>
+            <Header>
+                <img src={logoImg.src} alt="" />
+            </Header>
+
+            <Component {...pageProps }/>
+        </Container>
+    )
 }
 
